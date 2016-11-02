0104f87842bb92cb0aace479f9aaec41ad1d5a21
jrose-carecloud <jrose@carecloud.com>
2016-11-02 09:52:43 -0400

    Initial commit

---
+++ api.rb
@@ -0,0 +1,6 @@
+require 'httparty'
+require 'pp'
+
+response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Miami&appid=c38d29204353577f04f9a8b861e3c70e")
+pp response
+puts response["main"]["temp"]
